9131b87c27ccb11121905fac84b2a812a29ce962
Lisandrowalter <91380793+Lisandrowalter@users.noreply.github.com>
2022-06-21 22:19:55 -0300

    exerciciico 2

---
+++ exercicio_2.php
@@ -0,0 +1,10 @@
+<?php
+echo "digite um valor de 1 a 9 ";
+$n5 = readline();
+if($n5>=1 && $n5<=9){
+echo "o valor esta na faixa permitida";
+}else {
+    echo" o valor nao esta na faixa opermitida";
+}
+
+?>
