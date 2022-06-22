49f1d3fe65bde2ce66352578a53a7cc99f22f7fe
Lisandrowalter <91380793+Lisandrowalter@users.noreply.github.com>
2022-06-21 21:47:57 -0300

    sintaxe
    Sintaxe básica  php

---
+++ base_sintaxe.php
@@ -0,0 +1,31 @@
+<?php
+
+echo 'Parada do café!.';
+
+$nomeVariavel = 22;
+
+define ('NOME_CONSTANTE',10 );
+
+// Comentario de uma linha
+
+#  comentario  de uma linha
+
+/*
+varias linhas
+de comentarios
+*/
+
+/*
+Operadores aritmeticos
++ - * / %
+
+operadores relacionais
+> <  == ! =
+operadores logicos 
+AND OR NOT
+*/
+
+// terminal
+$variavel= readline ();
+
+?>
